c74afe1d99724582f1b27c7a757bfa394dbdf969
R.A Borja <raborja.dev@gmail.com>
2023-03-13 13:46:45 +0800

    update readme

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import babel from "vite-plugin-babel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ecommerce-product-page/',
   plugins: [react()],
 });
